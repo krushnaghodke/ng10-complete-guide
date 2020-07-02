67eccde06bbc8933bf7482cb837e209a410111a5
Krishna Ghodke <krishna.ghodke@quest-global.com>
2020-07-02 19:12:00 +0530

    shopping list changes

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+  //
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+  // another way to create above syntax
+
+  constructor(public name: string, public amount: number) {
+  }
+}
